662e2fa53249c344cbf17c2ca2f6cfdf99715075
kageurarenn <241020@std.hi-joho.ac.jp>
2025-06-04 10:11:48 +0900

    chpater8コード

---
+++ src/Hero.java
@@ -0,0 +1,23 @@
+public class Hero {
+    String name;
+    int hp;
+    public void sleep(){
+    this.hp =100;
+    System.out.println(this.name +"は、眠って回復した。");
+    }
+    public void sit(int sec){
+        this.hp += sec;
+        System.out.println(this.name+"は、"+sec+"秒座った!");
+        System.out.println("HPが"+sec+"ポイント回復した");
+    }
+    public void slip(){
+        this.hp -=50;
+        System.out.println(this.name+"は、転んで穴に落ち臓器を損傷した!");
+        System.out.println("50のダメージ!");
+    }
+    public void run(){
+        System.out.println(this.name+"は、逃げ出した!");
+        System.out.println("GAMEOVER");
+        System.out.println("最終HPは"+this.hp+"でした");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 //TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
-    public static void main(String[] args) {
-        //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
-        // IntelliJ IDEA によるその修正案を確認できます。
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args){
+        Hero h = new Hero();
+        h.name ="ミナト";
+        h.hp = 100;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
-            // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
-            System.out.println("i = " + i);
-        }
+        Matango m1 = new Matango();
+        m1.hp = 50;
+        m1.suffix ='A';
+
+        Matango m2 = new Matango();
+        m2.hp = 48;
+        m2.suffix ='B';
+        h.slip();
+        m1.run();
+        m2.run();
+        h.run();
     }
 }
